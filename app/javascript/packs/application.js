fa39a8e74290b7598a817d959e44a14e6ddb6e38
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2021-04-20 17:02:10 +0100

    First commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,26 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+console.log('Hello World from Webpacker')
+
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  const app = new Vue({
+    el: '#v-app',
+    data: {
+      message: "Can you say hello?"
+    },
+    components: { App }
+  })
+})
