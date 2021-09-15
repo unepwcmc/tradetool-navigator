e83576f6df34bf98603653e0a7f530c4aa9fe61b
Dagmara <dagmarakukla@gmail.com>
2021-09-15 15:43:26 +0100

    basic tailwind layout

--- app/javascript/components/TheHero.vue
+++ app/javascript/components/TheHero.vue
@@ -1,9 +1,9 @@
 <template>
   <header>
-    <div class="topbar">
+    <div class="bg-teal text-white px-20 py-10">
       logo
     </div>
-    <div class="hero">
+    <div class="hero px-40 py-40">
       <h1>{{ localeContent.title }}</h1>
       <p>{{ localeContent.intro }}</p>
     </div>
@@ -28,22 +28,22 @@ export default {
 </script>
 
 <style lang="scss">
-$grey-xdark: #1B2024;
+// $grey-xdark: #1B2024;
 
-body { 
-  margin: 0;
-}
+// body { 
+//   margin: 0;
+// }
 
-.topbar {
-  background-color: $grey-xdark;
-  color: #fff;
-  height: 100px;
-}
 
-.hero {
-  background-color: #ccc;
-  color: #fff;
-  height: 500px;
-}
+// .topbar {
+//   background-color: $grey-xdark;
+//   color: #fff;
+//   height: 100px;
+// }
 
+// .hero {
+//   background-color: #ccc;
+//   color: #fff;
+//   height: 500px;
+// }
 </style>
