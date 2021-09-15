59670c16a86eb9978327371c9bb35e39ef85fc9c
Dagmara <dagmarakukla@gmail.com>
2021-09-15 15:35:17 +0100

    move assets inside the vue app

--- app/javascript/components/TheFooter.vue
+++ app/javascript/components/TheFooter.vue
@@ -1,32 +1,57 @@
 <template>
-  <footer class="footer">
-    <p>Copyright 2021</p>
+  <footer class="px-20 py-10 flex justify-between bg-teal-dark text-white">
+    <div class="grid grid-rows-2 gap-y-10">
+      <span>Logo main</span>
+      <p>Copyright 2021</p>
+    </div>
+    <div class="min-w-1/2 grid grid-cols-3 gap-x-10">
+      <a v-for="logo in logos" v-bind:href="logo.url">
+        <img v-bind:src="logo.img"></span>
+      </a>
+    </div>
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'TheFooter'
-}
+  name: "TheFooter",
+  data() {
+    return {
+      logos: [
+        {
+          img: "../images/logo-main.png",
+          link: "https://unepwcmc.deltekfirst.com/UNEPWCMC/iAccess/",
+        },
+        {
+          img: "blabla",
+          link: "https://unepwcmc.deltekfirst.com/UNEPWCMC/iAccess/",
+        },
+        {
+          img: "https://s3.amazonaws.com/wcmc.logo/UNEP_WCMC_logo_white.svg",
+          link: "https://unepwcmc.deltekfirst.com/UNEPWCMC/iAccess/",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-$grey-xdark: #1B2024;
+// $grey-xdark: #1b2024;
 
-body { 
-  margin: 0;
-}
+// body {
+//   margin: 0;
+// }
 
-.topbar {
-  background-color: $grey-xdark;
-  color: #fff;
-  height: 100px;
-}
+// .topbar {
+//   background-color: $grey-xdark;
+//   color: #fff;
+//   height: 100px;
+// }
 
-.footer {
-  background-color: #ccc;
-  color: #fff;
-  padding: 40px 0;
-}
-
+// .footer {
+//   background-color: #ccc;
+//   color: #fff;
+//   padding: 40px 0;
+// }
 </style>
