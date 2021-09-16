254e0b7144e6835bdaf394682836c8cb47182d2a
Dagmara <dagmarakukla@gmail.com>
2021-09-16 17:52:27 +0100

    center footer items

--- app/javascript/components/TheFooter.vue
+++ app/javascript/components/TheFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="mt-auto center px-20 py-10 bg-teal-dark text-white">
     <div class="flex items-center sm:items-start flex-col sm:flex-row sm:flex-wrap sm:justify-between">
-      <div class="pb-6 w-full sm:w-2/5 xl:w-1/6 md:w-1/4 sm:grid grid-rows-2 sm:gap-y-12">
+      <div class="pb-6 w-3/5 sm:w-2/5 xl:w-1/6 md:w-1/4 sm:grid grid-rows-2 sm:gap-y-12">
         <a :href="mainLogo.link">
           <img v-bind:src="mainLogo.img" />
         </a>
@@ -15,7 +15,7 @@
         </a>
       </div>
     </div>
-    <p class="mt-8">Copyright 2021</p>
+    <p class="mt-8 text-center">Copyright 2021</p>
   </footer>
 </template>
 
