4c17806aa304bf3e85d42fdfcaaa2ef6a8f17638
Dagmara <dagmarakukla@gmail.com>
2021-09-16 12:39:33 +0100

    add links to logos

--- app/javascript/components/TheFooter.vue
+++ app/javascript/components/TheFooter.vue
@@ -11,13 +11,13 @@
     "
   >
     <div class="w-2/5 md:w-1/4 grid grid-rows-2 gap-y-12">
-      <a v-bind:href="mainLogo.url">
+      <a v-bind:href="mainLogo.link">
         <img v-bind:src="mainLogo.img" />
       </a>
       <p class="self-end">Copyright 2021</p>
     </div>
     <div class="w-2/5 grid md:grid-cols-3 gap-10">
-      <a v-for="logo in logos" :key="logo.img" v-bind:href="logo.url">
+      <a v-bind:href="logo.link" v-for="logo in logos" :key="logo.img">
         <img v-bind:src="logo.img" />
       </a>
     </div>
@@ -29,19 +29,19 @@ export default {
   name: "TheFooter",
   data() {
     return {
-      mainLogo: { img: require("../images/logo-main.png"), link: "" },
+      mainLogo: { img: require("../images/logo-main.png"), link: "https://tradehub.earth/" },
       logos: [
         {
           img: require("../images/logo-GCRF.png"),
-          link: "",
+          link: "https://www.ukri.org/our-work/collaborating-internationally/global-challenges-research-fund/",
         },
         {
           img: require("../images/logo-ukri.svg"),
-          link: "",
+          link: "https://www.ukri.org/",
         },
         {
           img: "https://s3.amazonaws.com/wcmc.logo/UNEP_WCMC_logo_white.svg",
-          link: "",
+          link: "https://www.unep-wcmc.org/",
         },
       ],
     };
