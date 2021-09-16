233f3622d0be8a58424e6da651e27b8d21594950
Dagmara <dagmarakukla@gmail.com>
2021-09-16 11:55:12 +0100

    style: add footer mobile display

--- app/javascript/components/TheFooter.vue
+++ app/javascript/components/TheFooter.vue
@@ -4,19 +4,19 @@
       mt-auto
       px-20
       py-10
-      flex flex-wrap
+      flex md:flex-wrap
       justify-between
       bg-teal-dark
       text-white
     "
   >
-    <div class="w-1/4 grid grid-rows-2 gap-y-12">
+    <div class="w-2/5 md:w-1/4 grid grid-rows-2 gap-y-12">
       <a v-bind:href="mainLogo.url">
         <img v-bind:src="mainLogo.img" />
       </a>
       <p class="self-end">Copyright 2021</p>
     </div>
-    <div class="w-2/5 grid grid-cols-3 gap-x-10">
+    <div class="w-2/5 grid md:grid-cols-3 gap-10">
       <a v-for="logo in logos" :key="logo.img" v-bind:href="logo.url">
         <img v-bind:src="logo.img" />
       </a>
