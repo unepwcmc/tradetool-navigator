a6dddcfcf78ce2ef615d087bc42979c92fbed229
Dagmara <dagmarakukla@gmail.com>
2021-09-17 10:47:15 +0100

    add other fornt weigths

--- app/javascript/components/TheHero.vue
+++ app/javascript/components/TheHero.vue
@@ -3,7 +3,7 @@
     <div class="bg-teal text-white p-10 sm:px-20">
       logo
     </div>
-    <div class="hero center p-10 sm:px-20">
+    <div class="w-full font-semibold hero center p-10 sm:px-20">
       <h1>{{ localeContent.title }}</h1>
       <p>{{ localeContent.intro }}</p>
     </div>
