1aa926ea4c5a84213f956f04d64d944d77bbcf20
Dagmara <dagmarakukla@gmail.com>
2021-09-17 11:55:58 +0100

    styles: Add hero background and padding

--- app/javascript/components/TheHero.vue
+++ app/javascript/components/TheHero.vue
@@ -3,9 +3,9 @@
     <div class="bg-teal text-white p-10 sm:px-20">
       logo
     </div>
-    <div class="w-full font-semibold hero center p-10 sm:px-20">
-      <h1>{{ localeContent.title }}</h1>
-      <p>{{ localeContent.intro }}</p>
+    <div class="w-full min-h-50 text-white bg-hero-pattern-1 bg-no-repeat bg-cover bg-left-center font-semibold hero center p-10 sm:px-20 md:px-40">
+      <h1 class="text-center my-10">{{ localeContent.title }}</h1>
+      <p class="my-10">{{ localeContent.intro }}</p>
     </div>
   </header>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,36 @@
 module.exports = {
-    purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-    darkMode: false,
-    theme: {
-        extend: {
-            colors: {
-                ash: {
-                    ligth: "#F7FAFB",
-                    DEFAULT: "#00000029",
-                    dark: "#707070",
-                },
-                teal: {
-                    light: "#2F4F5D",
-                    DEFAULT: "#1B2024",
-                    dark: "#1B1B24",
-                },
-                lime: { DEFAULT: "#AFB90F" },
-            },
-            fontFamily: {
-                'sans': ['Raleway', 'sans-serif', 'system-ui'],
-                // 'serif': ['ui-serif', 'Georgia', ...],
-                // 'mono': ['ui-monospace', 'SFMono-Regular', ...],
-                // 'display': ['Oswald', ...],
-                // 'body': ['"Open Sans"', ...],
-            }
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        ash: {
+          ligth: "#F7FAFB",
+          DEFAULT: "#00000029",
+          dark: "#707070",
         },
+        teal: {
+          light: "#2F4F5D",
+          DEFAULT: "#1B2024",
+          dark: "#1B1B24",
+        },
+        lime: { DEFAULT: "#AFB90F" },
+      },
+      fontFamily: {
+        sans: ["Raleway", "sans-serif", "system-ui"],
+        // 'serif': ['ui-serif', 'Georgia', ...],
+        // 'mono': ['ui-monospace', 'SFMono-Regular', ...],
+        // 'display': ['Oswald', ...],
+        // 'body': ['"Open Sans"', ...],
+      },
+      backgroundImage: {
+        "hero-pattern-1": "url('./images/hero-pattern-1.jpeg')",
+        "hero-pattern-2": "url('./images/hero-pattern-2.jpeg')",
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
