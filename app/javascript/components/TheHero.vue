ba29a5cb80c5dfb943110cc743199f8e79c6357e
Dagmara <dagmarakukla@gmail.com>
2021-09-26 17:02:45 +0100

    styles: add and apply overlay for hero

--- app/javascript/components/TheHero.vue
+++ app/javascript/components/TheHero.vue
@@ -8,7 +8,7 @@
           <img :src="logos.main.img" />
         </a>
     </div>
-    <div class="w-full min-h-96 flex flex-col items-center text-white bg-hero-pattern-1 bg-no-repeat bg-cover bg-left-center font-semibold">
+    <div class="w-full min-h-96 flex flex-col items-center text-white bg-hero-pattern-1 bg-no-repeat bg-cover bg-left-center font-semibold overlay">
       <h1 class="text-center my-10 w-3/5">{{ localeContent.title }}</h1>
       <h3 class="mb-10 w-3/5">{{ localeContent.intro }}</h3>
     </div>
