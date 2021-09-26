846e58f72e60d330dec5a1dfa8ba1c0e9a755ce8
Dagmara <dagmarakukla@gmail.com>
2021-09-26 17:40:55 +0100

    styles: add top and bottom padding to hero

--- app/javascript/components/TheHero.vue
+++ app/javascript/components/TheHero.vue
@@ -14,7 +14,7 @@
           {{logos.tradehub.text}}
         </a>
     </div>
-    <div class="w-full min-h-96 flex flex-col items-center text-white bg-hero-pattern-1 bg-no-repeat bg-cover bg-left-center font-semibold overlay">
+    <div class="w-full min-h-96 flex flex-col items-center text-white bg-hero-pattern-1 bg-no-repeat bg-cover bg-left-center font-semibold overlay py-6 sm:py-16">
       <h1 class="text-center my-10 w-3/5">{{ localeContent.title }}</h1>
       <h3 class="mb-10 w-3/5">{{ localeContent.intro }}</h3>
     </div>
