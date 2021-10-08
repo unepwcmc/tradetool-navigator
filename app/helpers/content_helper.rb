e8fa9042a9a59182d0f2af5bbf4e9b7bafbb9ef7
Dagmara <dagmarakukla@gmail.com>
2021-10-08 13:27:46 +0100

    metatags start

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -9,4 +9,11 @@ module ContentHelper
       yaml["#{locale}"]["#{filename}"].to_json
     end
   end
+  
+  def get_content_value filename, partial, content_key
+    if(filename != nil)
+      yaml = load_yaml("#{content_base_path}/#{filename}/#{locale}.yml")
+      yaml["#{locale}"]["#{filename}"]["#{partial}"]["#{content_key}"]
+    end
+  end
 end
