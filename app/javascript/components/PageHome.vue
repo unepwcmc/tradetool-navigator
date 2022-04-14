d4fe1c22e14701f0b97b982653302184f5029c61
maxime <maxycle@hotmail.com>
2022-04-14 16:13:01 +0100

    refactor: format file

--- app/javascript/components/PageHome.vue
+++ app/javascript/components/PageHome.vue
@@ -1,82 +1,82 @@
 <template>
-	<div
-		class="min-h-screen w-full grid font-sans text-xs sm:text-sm md:text-base"
-	>
-		<TheHero :localeContent="localeContent.hero" :logos="logos" />
-		<TheTextBlock
-			:background="'bg-ash'"
-			:color="'black'"
-			:text="textAttribution"
-		/>
-		<slot />
-		<TheTextBlock :background="'bg-ash-light'" :text="textFeedback" />
-		<TheFooter :logos="logos" />
-	</div>
+  <div
+    class="min-h-screen w-full grid font-sans text-xs sm:text-sm md:text-base"
+  >
+    <TheHero :localeContent="localeContent.hero" :logos="logos" />
+    <TheTextBlock
+      :background="'bg-ash'"
+      :color="'black'"
+      :text="textAttribution"
+    />
+    <slot />
+    <TheTextBlock :background="'bg-ash-light'" :text="textFeedback" />
+    <TheFooter :logos="logos" />
+  </div>
 </template>
 
 <script>
-	import TheHero from './TheHero.vue'
-	import TheTextBlock from './TheTextBlock.vue'
-	import TheFooter from './TheFooter.vue'
-	import logos from '../data/logos.js'
-	import { createNamespacedHelpers } from 'vuex'
-	const { mapActions, mapGetters } = createNamespacedHelpers('filterableTable')
+import TheHero from './TheHero.vue'
+import TheTextBlock from './TheTextBlock.vue'
+import TheFooter from './TheFooter.vue'
+import logos from '../data/logos.js'
+import { createNamespacedHelpers } from 'vuex'
+const { mapActions, mapGetters } = createNamespacedHelpers('filterableTable')
 
-	export default {
-		components: {
-			TheTextBlock,
-			TheFooter,
-			TheHero
-		},
+export default {
+  components: {
+    TheTextBlock,
+    TheFooter,
+    TheHero
+  },
 
-		props: {
-			localeContent: {
-				type: Object
-			},
-			textFeedback: {
-				type: String
-			},
-			textAttribution: {
-				type: String
-			}
-		},
+  props: {
+    localeContent: {
+      type: Object
+    },
+    textFeedback: {
+      type: String
+    },
+    textAttribution: {
+      type: String
+    }
+  },
 
-		created() {
-			this.$root.$on('openModal', this.updateModalTitle)
-		},
+  created() {
+    this.$root.$on('openModal', this.updateModalTitle)
+  },
 
-		computed: {
-			...mapGetters(['options', 'modalContent'])
-		},
+  computed: {
+    ...mapGetters(['options', 'modalContent'])
+  },
 
-		data: () => ({
-			logos: logos
-		}),
+  data: () => ({
+    logos: logos
+  }),
 
-		methods: {
-			...mapActions(['updateOptions']),
+  methods: {
+    ...mapActions(['updateOptions']),
 
-			updateModalTitle({ tableId }) {
-				const modalContent = this.modalContent(tableId)
-				const modalTitle = modalContent.find(contentItem => {
-					return contentItem.name === 'name'
-				}).value
+    updateModalTitle({ tableId }) {
+      const modalContent = this.modalContent(tableId)
+      const modalTitle = modalContent.find((contentItem) => {
+        return contentItem.name === 'name'
+      }).value
 
-				const oldOptions = this.options(tableId)
-				
-				const optionsConfiguration = {
-					tableId,
-					options: {
-						...oldOptions,
-						modal: {
-							...oldOptions.modal,
-							title: modalTitle,
-						}
-					}
-				}
+      const oldOptions = this.options(tableId)
 
-				this.updateOptions(optionsConfiguration)
-			}
-		}
-	}
+      const optionsConfiguration = {
+        tableId,
+        options: {
+          ...oldOptions,
+          modal: {
+            ...oldOptions.modal,
+            title: modalTitle
+          }
+        }
+      }
+
+      this.updateOptions(optionsConfiguration)
+    }
+  }
+}
 </script>
