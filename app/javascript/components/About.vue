e7b74303a0770ca229713c57cb14d3b8b10ee578
maxime <maxycle@hotmail.com>
2022-04-19 16:00:57 +0100

    feat: add about page text

--- app/javascript/components/About.vue
+++ app/javascript/components/About.vue
@@ -1,11 +1,39 @@
 <template>
-  <div class="p-14">
-    This is the ABOUT page
+  <div class="p-14 text-white bg-teal">
+    <h1>About</h1><br>
+    <p>
+      The Trade Tools Navigator seeks to bring together existing initiatives in
+      one place to help users understand the impacts of production and
+      consumption on nature. The aim of the navigator is to add value to
+      existing work and synthesize the tools currently available which are
+      relevant to trade, impacts of trade, and commodity supply chains
+    </p>
+    <br />
+    <p>
+      Tools which are included give information on a variety of aspects
+      including:
+    </p>
+    <br />
+    <ul class="pl-4">
+      <li>Overview of state of nature</li>
+      <li>Impacts (direct and indirect) – especially negative impacts</li>
+      <li>Drivers of impact</li>
+      <li>Spatialised information</li>
+      <li>Trade flows or Maps Map or list trade flows</li>
+      <li>Solutions (including frameworks, best practices)</li>
+    </ul><br>
+    <h2>Data and Methodology</h2><br>
+    <p>
+      The data found in the Trade Tools Navigator is from existing databases
+      from UNEP-WCMC from previous data inventories complied on supply chains,
+      and trade. Online reviews and searches were and continue to be carried out
+      for each commodity covered in the GCRF Trade Development and the
+      Environment Hub using Google and key document searches, with standardised
+      search terms, and targeted website searches.
+    </p>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
