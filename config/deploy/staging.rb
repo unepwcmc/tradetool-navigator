7548a2cea78daa87a93bffc29e4bf72fbd0b5dab
strtwtsn <stuart.watson@unep-wcmc.org>
2023-03-28 11:49:36 +0100

    Updated staging url

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@ set :branch, 'develop'
 server 'web-supported-staging.linode.unep-wcmc.org', user: 'wcmc', roles: %w[app web db]
 
 set :application, 'tradehub-navigator'
-set :server_name, 'tradehub-navigator.web-supported-staging.linode.unep-wcmc.org'
+set :server_name, 'tradetools-navigator.web-supported-staging.linode.unep-wcmc.org'
 set :sudo_user, 'wcmc'
 set :app_port, '80'
 
